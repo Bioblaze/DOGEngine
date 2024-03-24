7afed75285eaca88d53e0cfe6b63517e3a57647d
Randolph W. Aarseth II <bioblazepayne@gmail.com>
2024-03-23 17:07:20 -0700

    Almost forgot to add x, y, z positioning. Now added.

--- DOGEngine/Core/GameEntity.cpp
+++ DOGEngine/Core/GameEntity.cpp
@@ -6,3 +6,13 @@ GameEntity::GameEntity(entt::registry& registry)
 GameEntity::~GameEntity() {
     registry.destroy(entity);
 }
+
+void GameEntity::setPosition(float x, float y, float z) {
+    posX = x;
+    posY = y;
+    posZ = z;
+}
+
+std::tuple<float, float, float> GameEntity::getPosition() const {
+    return { posX, posY, posZ };
+}
