976b5d250248d0c754cee3549e4dc2cf2916c7fe
Randolph W. Aarseth II <bioblazepayne@gmail.com>
2024-03-31 18:50:48 -0700

    added g++ as a cmake compiler

--- Build.sh
+++ Build.sh
@@ -2,6 +2,6 @@
 mkdir -p build
 
 cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=/usr/local/share/vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake .. -DCMAKE_TOOLCHAIN_FILE=/usr/local/share/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_CXX_COMPILER=g++
 cmake --build .
 cd ..
