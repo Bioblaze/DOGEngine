53404ece6315389fff7d00ff378f3e55d53e3446
Randolph W. Aarseth II <bioblazepayne@gmail.com>
2024-03-31 18:43:13 -0700

    modifying to try and get vcpkg working on linux

--- Build.sh
+++ Build.sh
@@ -2,6 +2,6 @@
 mkdir -p build
 
 cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake .. -DCMAKE_TOOLCHAIN_FILE=/usr/local/share/vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build .
 cd ..
