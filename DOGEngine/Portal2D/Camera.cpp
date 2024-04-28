e19063f26127ddf7525ee0f1c3102dbd427522c9
Julio Meroño Sáez <segfaultdev@proton.me>
2024-04-28 02:41:43 +0200

    Portal2D/Camera.cpp: started working on a proper collision system.
    The code is not fully functional as of now, though the math is mostly
    there. Also, most of the old room changing code was removed.

--- DOGEngine/Portal2D/Camera.cpp
+++ DOGEngine/Portal2D/Camera.cpp
@@ -6,9 +6,6 @@
 #include <cstdio>
 
 void Portal2D::Camera::Update() {
-    // TODO: check if camera got outside its room and update
-    // this->room accordingly.
-    
     const float radius = 0.125f;
     
     const float x2 = this->old_point_x;
@@ -51,21 +48,39 @@ void Portal2D::Camera::Update() {
         // Now, let the magic begin!
         
         if (wall.link == nullptr) {
-            const float cos_d = sqrtf(((x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1)) * ((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3)));
+            // We want the normal, so we invert the components and negate
+            // one of them.
             
-            if (cos_d == 0.0f) {
+            const float n_length = sqrtf((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0));
+            const float d_length = sqrtf((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
+            
+            const float nx = (y1 - y0) / n_length;
+            const float ny = (x0 - x1) / n_length;
+            
+            const float dx = (x3 - x2) / d_length;
+            const float dy = (y3 - y2) / d_length;
+            
+            if (n_length == 0.0f || d_length == 0.0f) {
                 continue;
             }
             
-            const float cos_x = fabsf(d) / cos_d;
+            const float cos_angle = nx * dx + ny * dy;
             
-            const float b_delta = (radius / cos_x) / sqrtf((x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1));
-            const float b_final = b - 0.0f * b_delta;
-            
-            if (b_final > 0.0f && b_final < 1.0f) {
-                x3 = x2 + (x3 - x2) * b_final * 0.5f;
-                y3 = y2 + (y3 - y2) * b_final * 0.5f;
+            if (cos_angle == 0.0f) {
+                continue;
             }
+            
+            const float d_radius = radius / cos_angle;
+            const float b_radius = d_radius / d_length;
+            
+            const float b_final = b - b_radius;
+            
+            if (b < 0.0f || b > 1.0f) {
+                continue;
+            }
+            
+            x3 = x2 + (x3 - x2) * b_final;
+            y3 = y2 + (y3 - y2) * b_final;
         } else {
             if (b < 0.0f || b > 1.0f) {
                 continue;
@@ -75,83 +90,6 @@ void Portal2D::Camera::Update() {
             break;
         }
     }
-    
-    /*
-    float cx = this->old_point_x;
-    float cy = this->old_point_y;
-    
-    float delta_cx = this->point_x - cx;
-    float delta_cy = this->point_y - cy;
-    
-    float cd = sqrtf(delta_cx * delta_cx + delta_cy * delta_cy);
-    
-    if (cd == 0.0f) {
-        return;
-    }
-    
-    delta_cx /= cd;
-    delta_cy /= cd;
-    
-    while (cd > 0.0f) {
-        float step = std::min(cd, radius * 0.0625f);
-        cd -= step;
-        
-        cx += delta_cx * step;
-        cy += delta_cy * step;
-        
-        for (auto i = 0; i < this->room->walls.size(); i++) {
-            const Portal2D::Wall &wall = this->room->walls[i];
-            auto j = (i + 1) % this->room->walls.size();
-            
-            // (x0, y0) - (x1, y1) defines the wall as it exists.
-            
-            float x0 = wall.point_x;
-            float y0 = wall.point_y;
-            
-            float x1 = this->room->walls[j].point_x;
-            float y1 = this->room->walls[j].point_y;
-            
-            // Calculate the distance from the camera to the wall, to check
-            // for possible collisions.
-            
-            float a = (x0 * x0 + y0 * y0 + x1 * cx + y1 * cy - x0 * (x1 + cx) - y0 * (y1 + cy));
-            float b = (x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1);
-            
-            if (b == 0.0f) {
-                continue;
-            }
-            
-            float k = std::max(std::min(a / b, 1.0f), 0.0f);
-            
-            float px = x0 + (x1 - x0) * k;
-            float py = y0 + (y1 - y0) * k;
-            
-            float nx = cx - px;
-            float ny = cy - py;
-            
-            float d = sqrtf(nx * nx + ny * ny);
-            
-            nx /= d;
-            ny /= d;
-            
-            if (wall.link == nullptr) {
-                if (d < radius) {
-                    cx += nx * (radius - d);
-                    cy += ny * (radius - d);
-                }
-            } else {
-                if (d < 0.0f) {
-                    // this->room = wall.link;
-                    // break;
-                }
-            }
-        }
-        
-    }
-    
-    this->point_x = this->old_point_x = cx;
-    this->point_y = this->old_point_y = cy;
-    */
     
     this->old_point_x = this->point_x = x3;
     this->old_point_y = this->point_y = y3;
